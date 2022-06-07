6aa263e93a3b046e3cc09d52c42dfcaebaaf33b2
Jonathan Sugihara <jonathansugihara@Jonathans-MacBook-Pro.local>
2022-06-06 20:43:02 -0600

    weather merge

--- assets/script.js
+++ assets/script.js
@@ -92,5 +92,66 @@ function backgroundImage() {
 backgroundImage();
 
 
-
+var tableBody = document.getElementById('hourly-table');
+function getCurrentWeatherApi(position) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=f3c7f6f0c1bd55045c6c4f5cb473b0de&units=imperial`;
+    // console.log(position)
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var currentTemp = (data.main.temp)
+            var currenthWeatherHeader = document.getElementById('currentWeather');
+            currenthWeatherHeader.innerHTML = `Currently ${Math.round(currentTemp)}° F`
+        });
+}
+function getHourlyWeatherApi(position) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lon=${position.coords.longitude}&appid=f3c7f6f0c1bd55045c6c4f5cb473b0de&units=imperial&lat=${position.coords.latitude}&exclude=minutely`;
+    // console.log(requestUrl)
+    fetch(requestUrl)
+    .then(function (response) {
+    console.log(requestUrl)
+        return response.json();
+    })
+    .then(function (response) {
+        // console.log(response)
+    for (let i = 0; i <= 5; i++) {
+        var currentForcastItem = response.hourly[i];
+        console.log(currentForcastItem);
+        var createTableRow = document.createElement('tr')
+        var createTableData = document.createElement('td')
+        var createTableDataTemp = document.createElement('td')
+        var createTableDataWindSpeed = document.createElement('td')
+        var timeText = document.createElement('p')
+        var tempText = document.createElement('p')
+        var windSpeed = document.createElement('p')
+        let unix_timestamp = currentForcastItem.dt
+        timeStamp = new Date(unix_timestamp * 1000)
+        var hours = timeStamp.getHours()
+        var minutes = "0" + timeStamp.getMinutes()
+        var AmOrPm = hours >= 12 ? 'pm' : 'am';
+        hours = (hours % 12) || 12;
+        var formattedTime = hours + ':' + minutes.substr(-2) + " " + AmOrPm
+        // console.log(formattedTime)
+        timeText.innerHTML = formattedTime
+        tempText.innerHTML = `${Math.round(currentForcastItem.temp)}° F `
+        windSpeed.innerHTML = `${(currentForcastItem.wind_speed)} MPH`
+        createTableData.appendChild(timeText)
+        createTableDataTemp.appendChild(tempText)
+        createTableDataWindSpeed.appendChild(windSpeed)
+        createTableRow.appendChild(createTableData)
+        createTableRow.appendChild(createTableDataTemp)
+        createTableRow.appendChild(createTableDataWindSpeed)
+        tableBody.appendChild(createTableRow)
+    }
+    })
+}
+function callCurrentAndHourlyWeather(position){
+    getCurrentWeatherApi(position)
+    getHourlyWeatherApi(position)
+}
+$(document).ready(function () {
+    navigator.geolocation.getCurrentPosition(callCurrentAndHourlyWeather)
+});
 
